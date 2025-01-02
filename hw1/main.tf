7939d1767863aa356aa9b437a6f6257cf9865b92
Serhii Vykaliuk <serhii.v@gmail.com>
2025-01-02 22:26:47 +0200

    terraform zero

--- hw1/main.tf
+++ hw1/main.tf
@@ -12,25 +12,3 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  tags         = ["web", "dev", "hw0"]
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
